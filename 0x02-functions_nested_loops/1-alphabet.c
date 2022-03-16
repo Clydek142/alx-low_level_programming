1f3a5088aca118d791ea88d6d2727a2b7612befb
Clydek142 <clydek142@gmail.com>
2022-03-16 04:44:35 -0700

    1 alphabet.c commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *  * main - prints the alphabet
+ *   *
+ *    * Return: Always 0 Success
+ *     */
+void print_alphabet(void)
+{
+	int ch('a');
+	while (ch <='z')
+	{
+		putchar(ch);
+		char++;
+	}
+	putchar('\n');
+}
