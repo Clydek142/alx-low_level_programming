67384594b6fe2fb74d8463c988761244f5c84ea4
Clydek142 <clydek142@gmail.com>
2022-03-24 03:59:46 -0700

    0 -strcat commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
+int _strcnp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif
+
